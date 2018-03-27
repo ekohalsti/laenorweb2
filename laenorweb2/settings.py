306cae972ebd23265f0faf6a0d1187df24fec328
Esa Halsti <esa.halsti@tanktwo.com>
2018-03-27 21:53:48 +0300

    add django filters to installed apps and local_settings importer. Use local_settings.py to import sensitive information, such as passwords

--- laenorweb2/settings.py
+++ laenorweb2/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'tag',
 ]
 
@@ -124,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
