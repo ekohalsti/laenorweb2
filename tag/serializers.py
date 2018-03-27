bb50fa55f25521420013a9e0aad7a2042765c064
Esa Halsti <esa.halsti@tanktwo.com>
2018-03-27 21:38:06 +0300

    add initial serializer & views for models

--- tag/serializers.py
+++ tag/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
-from .models import Tag
+from .models import Tag, TagGroup, Note, NoteText
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
+        fields = '__all__'
+
+
+class TagGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TagGroup
+        fields = '__all__'
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = '__all__'
+
+
+class NoteTextSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NoteText
         fields = '__all__'
--- tag/views.py
+++ tag/views.py
@@ -1,11 +1,28 @@
 import django_filters
 from rest_framework import viewsets
-from .serializers import TagSerializer
-from .models import Tag
+from .serializers import TagSerializer, TagGroupSerializer, NoteSerializer, NoteTextSerializer
 
 
 # Create your views here.
 class TagView(viewsets.ModelViewSet):
     serializer_class = TagSerializer
     filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
-    filter_fields = ('__all__')
+    filter_fields = '__all__'
+
+
+class TagGroupView(viewsets.ModelViewSet):
+    serializer_class = TagGroupSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_fields = '__all__'
+
+
+class NoteView(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_fields = '__all__'
+
+
+class NoteTextView(viewsets.ModelViewSet):
+    serializer_class = NoteTextSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_fields = '__all__'
